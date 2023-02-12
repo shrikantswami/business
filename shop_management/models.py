f0a7de9a4f7fef5b87ffe7b96da8874d72ddf50c
Shubhada1995 <shubhadahundekar95@gmail.com>
2023-02-13 00:07:21 +0530

    added utility & shop django app also added product table details

--- shop_management/models.py
+++ shop_management/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class ProductDetail(models.Model):
+    """
+    This table contains product  details
+    """
+    product_id = models.BigAutoField(db_index=True, primary_key=True)
+    name = models.TextField( max_length=255, unique=True,null=True, blank=True)
+    price = models.IntegerField(null=True, blank=True)
+    details = models.CharField( max_length=255, null=True, blank=True)
+    quantity = models.IntegerField(null=True, blank=True)
+    stock = models.IntegerField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
