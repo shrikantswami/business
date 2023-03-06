c4008cf80f1cb60c9713b89950dcc51e4b5609ae
Shubhada1995 <shubhadahundekar95@gmail.com>
2023-03-07 00:48:57 +0530

    api path updated

--- shop_management/urls.py
+++ shop_management/urls.py
@@ -4,5 +4,5 @@ from . import product_details
 
 urlpatterns = [
     # path('product_list', product_details.get_list_of_products, name='product_list'),
-    path('add_product_details', product_details.ProductDetails.as_view(), name='add_product_details'),
+    path('product_details', product_details.ProductDetails.as_view(), name='add_product_details'),
 ]
